b4e8729a6843ccf27b56f358a2172d58da30e8b7
Vinicius Saueia <viniciussaueia@digix.com.br>
2021-07-09 13:59:47 -0400

    avaliacoes iniciais

--- Sorteio/Familia/Renda.cs
+++ Sorteio/Familia/Renda.cs
@@ -4,7 +4,14 @@ namespace Sorteio.Familia
 {
     public class Renda
     {
+        /**
+         * Encapsulamento dos sets
+         * Não precisa do identificar para familia
+         * Não precisa validar o identificar de familia
+         * O nome "RendaDaPessoa" não é claro, esse é o valor da renda
+         */
         public decimal RendaDaPessoa { get; set; }
+        
         public Guid IdentificadorDaPessoa { get; set; }
 
         public Renda(decimal renda)
